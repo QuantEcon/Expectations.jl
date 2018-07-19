8a6671bc0f45c6d4a5944f1af79db2feb1f13ad1
Arnav Sood <soodarnav01@gmail.com>
2018-07-19 12:27:50 -0700

    unnecessary comment

--- test/runbenchmarks.jl
+++ test/runbenchmarks.jl
@@ -7,7 +7,7 @@ benchmarks = BenchmarkGroup()
 #Put in specific benchmarks
 dist1 = DiscreteUniform(1, 10)
 E1 = expectation(dist1)
-benchmarks["discreteuniform"] = @benchmarkable E1($(x -> x)) # $E1 is optional.
+benchmarks["discreteuniform"] = @benchmarkable E1($(x -> x))
 dist2 = Normal()
 E2 = expectation(dist2)
 benchmarks["standardnormal"] = @benchmarkable E2($(x -> x))
