d79beee057021a231e12db23e581a67d95ec244b
Arnav Sood <soodarnav01@gmail.com>
2018-07-19 12:27:13 -0700

    add benchmarks

---
+++ test/runbenchmarks.jl
@@ -0,0 +1,30 @@
+# Import dependency. 
+using BenchmarkTools, Expectations, Distributions
+
+#Create benchmark group and benchmarks
+benchmarks = BenchmarkGroup()
+
+#Put in specific benchmarks
+dist1 = DiscreteUniform(1, 10)
+E1 = expectation(dist1)
+benchmarks["discreteuniform"] = @benchmarkable E1($(x -> x)) # $E1 is optional.
+dist2 = Normal()
+E2 = expectation(dist2)
+benchmarks["standardnormal"] = @benchmarkable E2($(x -> x))
+dist3 = Exponential(2)
+E3 = expectation(dist3)
+benchmarks["exponential"] = @benchmarkable E3($(x -> x))
+#...
+
+results = run(benchmarks) # Get results. 
+results = median(results) # Condense to median. 
+
+# To save results, manually call in the REPL: BenchmarkTools.save("benchmarks.json", results)
+
+#Compare to old results
+try
+  oldresults= BenchmarkTools.load("benchmarks.json")[1]
+  judge(oldresults, results)
+catch err
+  error("Couldn't load file- make sure that you've previously saved results.", err.prefix)
+end
