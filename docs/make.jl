e429a92e43efa8ff1c0b78029ed204a5ff07550a
Arnav Sood <soodarnav01@gmail.com>
2018-07-20 13:20:53 -0700

    add deps to deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,8 @@ makedocs()
 
 # Push the documentation to the server.
 deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/econtoolkit/Expectations.jl.git",
-    julia = "nightly"
+    julia = "nightly",
+    osname = "linux"
 )
