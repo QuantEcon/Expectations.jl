f947bf371ca21a1556a7f3e215bab0127a1c6c1b
Arnav Sood <me@arnavsood.com>
2019-07-23 18:10:54 -0400

    remove deprecated kwargs from deploydocs() call

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,4 @@ makedocs(sitename = "Expectations.jl")
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/econtoolkit/Expectations.jl.git",
-    julia = "1.0",
-    osname = "linux"
 )
