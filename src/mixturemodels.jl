c957ce78b60a404ef654e499653051f8953d6d47
Arnav Sood <5009477+arnavs@users.noreply.github.com>
2020-03-14 21:02:53 -0700

    add methods

--- src/mixturemodels.jl
+++ src/mixturemodels.jl
@@ -14,3 +14,6 @@ expectations(e::MixtureExpectation) = e.expectations
 expectation(f::Function, m::UnivariateMixture; kwargs...) = dot(probs(m), [expectation(f, dist; kwargs...) for dist in components(m)])
 *(r::Real, e::MixtureExpectation) = MixtureExpectation(r*expectations(e), weights(e))
 # *(e::MixtureExpectation, h::AbstractArray) = dot(weights(e), [E*h for E in expectations(e)])
+import Base.+
++(e1::IterableExpectation, e2::IterableExpectation) = MixtureExpectation([e1, e2], [1.0, 1.0])
++(expectations...) = MixtureExpectation(expectations, ones(length(expectations)))
